45d7c4a55bf2522d9f9a0d5a772ed2a27520f5a3
Pablo Couto <pablocouto.senai564@gmail.com>
2023-04-18 14:29:34 -0300

    alteracao em heron

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function heron() {
     if (a > 0 && b > 0 && c > 0) {
     let p = (a+ b+ c)/2;
 
-    let area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    let area = Math.sqrt(p*((p-a)*(p-b)*(p-c)));
 
     document.getElementById("perimetro").innerHTML = `O perímetro do triângulo é: ${a+b+c}`;
     document.getElementById("area").innerHTML = `A área do triângulo é: ${area}`;
@@ -15,10 +15,10 @@ function heron() {
     }
     
     if (a == b ==  c){
-        document.getElementById("tipo").value = "Equilátero";
+        document.getElementById("tipo").innerHTML = "Tipo: Equilátero";
     } else if ( a == b !=  c ||  a != b == c){
-        document.getElementById("tipo").value = "Isosceles";
+        document.getElementById("tipo").innerHTML = "Tipo: Isosceles";
     } else {
-        document.getElementById("tipo").value = "Escaleno";
+        document.getElementById("tipo").innerHTML = "Tipo: Escaleno";
     }
 }
