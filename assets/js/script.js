01488cebe48868b84763a6d49491ff58d63a53c4
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-04-18 15:31:54 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,14 @@ function heron() {
     if (a > 0 && b > 0 && c > 0) {
     let p = (a + b + c)/2;
 
+    /*fórmula de heron*/
+
     let area = Math.sqrt(p*((p-a)*(p-b)*(p-c)));
         if ( a + b <= c || b < a + c || c < a + b) {
             document.getElementById("area").innerHTML = "triangulo inválido";
             return; 
         }
-
+        /* verificação se é impar ou par ~*/
         if (area % 2 == 0 && p % 2 == 0) {
             document.getElementById("perimetro").innerHTML = `O perímetro do triângulo é: ${a+b+c} (par)`;
             document.getElementById("area").innerHTML = `A área do triângulo é: ${area} (par)`;
@@ -27,12 +29,11 @@ function heron() {
             document.getElementById("perimetro").innerHTML = `O perímetro do triângulo é: ${a+b+c} (ímpar)`;
             document.getElementById("area").innerHTML = `A área do triângulo é: ${area} (ímpar)`;
         }
-    }
-
-    else {
+    /* caso não insira valores */
+    }  else {
         alert("Insira valores válidos (positivos e diferentes de 0");
     }
-
+    /* definir triangulo */
     if (a > 0 && b > 0 && c > 0) {
     if (a == b && b == c){
         document.getElementById("tipo").innerHTML = "O triângulo é do tipo equilátero!";
