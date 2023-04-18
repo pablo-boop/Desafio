68d8e7807fddb22dbb39a748ee30fad1b3fa9192
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-04-18 14:03:30 -0300

    oloco

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function heron () {
+    let a = Number(document.getElementById("").value);
+    let b = Number(document.getElementById("").value);
+    let c = Number(document.getElementById("").value);
+
+    if (a > 0 && b > 0 && c > 0){
+    let p = (a+ b+ c)/2
+
+    let area = Math.sqrt(p*(p-a)*(p-b)*(p-c))
+
+    document.getElementById("").innerHTML = `O perímetro do triângulo é ${a+b+c} e a área é: ${area}`
+    } else {
+        alert("Insira valores válidos (positivos e diferentes de 0")
+    }
+}
